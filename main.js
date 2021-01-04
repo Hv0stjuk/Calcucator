2c354c06996d47786023df559aa5d4f8e39e50cb
Hv0stjuk <wot.hvostjuk@gmail.com>
2021-01-04 18:05:57 +0300

    feat: add sqrt, pow, abs, backspace

--- main.js
+++ main.js
@@ -5,38 +5,57 @@ const clearBtn = document.querySelectorAll('.clear');
 const percentBtn = document.getElementById('percent');
 const plusminusBtn = document.getElementById('plusminus');
 const display = document.getElementById('display');
+const btnSwitchCalc = document.getElementById('engineering');
+const addBtnEngineering = document.querySelectorAll('.engineering--hiden');
+const allBtn = document.querySelectorAll('.button');
+const action = document.getElementById('actions');
+
+const backspace = document.getElementById('backspace');
+const special = document.querySelectorAll('.engineering--hiden');
+
 let MemoryCurrentNumber = 0;
 let MemoryNewNumber = false;
 let MemoryPendingOperation = '';
 
+special.forEach((element) => {
+  element.addEventListener('click', ({target}) => {
+    operation(target.id);
+});
+});
+
+backspace.addEventListener('click', () => {
+  MemoryNewNumber = false;
+  MemoryCurrentNumber = display.value.slice(0, display.value.length - 1);
+  if (MemoryCurrentNumber === '') {
+    MemoryCurrentNumber = 0;
+  }
+  display.value = MemoryCurrentNumber;
+});
+
+btnSwitchCalc.addEventListener('click', () => {
+  addBtnEngineering.forEach(el => el.classList.toggle('engineering--hiden'));
+  allBtn.forEach(el => el.classList.toggle('button--small'));
+});
+
 numberBtn.forEach((element) => {
-  element.addEventListener('click', (e) => {
-    numberPress(e.target.innerText);
+  element.addEventListener('click', ({target}) => {
+    numberPress(target.innerText);
   });
 });
 
 operationBtn.forEach((element) => {
-  element.addEventListener('click', (e) => {
-    operation(e.target.innerText);
+  element.addEventListener('click', ({target}) => {
+    operation(target.innerText);
   });
 });
 
 decimalBtn.addEventListener('click', decimal);
 
 clearBtn.forEach((element) => {
-  element.addEventListener('click', (e) => {
-    clear(e.target.id);
+  element.addEventListener('click', ({target}) => {
+    clear(target.id);
   });
 });
-
-percentBtn.addEventListener('click', (e) => {
-  
-});
-
-plusminusBtn.addEventListener('click', (e) => {
-  plusminus(display.value[0]);
-});
-
 
 function numberPress(number) {
  if (MemoryNewNumber) {
@@ -68,12 +87,19 @@ function operation(op) {
       MemoryCurrentNumber *= parseFloat(localOperationMemory);
     } else if (MemoryPendingOperation === '/') {
       MemoryCurrentNumber /= parseFloat(localOperationMemory);
+    } else if (op === 'sqrt') {
+      MemoryCurrentNumber = Math.sqrt(parseFloat(localOperationMemory));
+    } else if (op === 'module') {
+      MemoryCurrentNumber = Math.abs(parseFloat(localOperationMemory));
+    } else if (op === 'exp') {
+      MemoryCurrentNumber = Math.pow(parseFloat(localOperationMemory), 2);
     } else {
       MemoryCurrentNumber = parseFloat(localOperationMemory);
-    };
+    }
     display.value = MemoryCurrentNumber;
     MemoryPendingOperation = op;
   }
+  writeAction (op);
 };
 
 function decimal() {
@@ -90,6 +116,7 @@ function decimal() {
 };
 
 function clear(id) {
+
     if (id === 'ce') {
       display.value = '0';
       MemoryNewNumber = true;
@@ -99,10 +126,21 @@ function clear(id) {
       MemoryNewNumber = true;
       MemoryCurrentNumber = 0;
       MemoryPendingOperation = 0;
-    };
+    }
 };
 
+function writeAction (op) {
+  if (op !== '=' && op !== 'module' && op !== 'sqrt' && op !== 'exp' && op !== 'backspace') {
+    action.textContent = `${op} `;
+  } else if (display.value === '0' || op === 0) {
+    action.textContent = '';
+  } else if (op === 'module') {
+    action.textContent = `|| `;
+  } else if (op === 'exp') {
+    action.textContent = 'х²';
+  } else if (op === 'sqrt') {
+    action.textContent = '√'
+  }
 
-function plusminus(symbol) {
+};
 
-}
